afd594cee86b834ddfc6c07b896c027d3028a6e5
axrod72 <113714067+axrod72@users.noreply.github.com>
2025-12-19 10:26:04 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Kenny Bot',
-  author: 'Wambugu Kinyua',
-  botName: "Kinyua Bot",
-  botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Dolphin Bot',
+  author: 'Alex',
+  botName: "Dolphin Bot",
+  botOwner: 'AlexDolphins72', // Your name
+  ownerNumber: '+52 9981530137', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
